7040a436f4a5d2853bf67f6bc6efd0b7d452b0e4
KnittingBatman <kscala648@gmail.com>
2017-03-03 10:59:24 -0500

    Create Body.py

---
+++ Body.py
@@ -0,0 +1,55 @@
+#Creates a body. Accepts user inputs for name and gender.
+
+class Body():
+
+    #winds flesh and blood and bone
+    #viscera align
+    #a program executed cell by cell
+    def __init__(self, name, gender):
+        self.build()
+        self.name = name
+        self.gender = gender
+            #seeds of an identity are planted
+        self.brain = []
+        self.ears = []
+        self.eyes = []
+        self.heart = []
+        self.intestines = []
+        self.kidneys = []
+        self.limbs = []
+        self.liver = []
+        self.lungs = []
+        self.nose = []
+        self.skin = []
+        self.spleen = []
+        self.stomach = []
+        self.tongue = []
+        self.alive = True
+            #this is new.
+            #I am new.
+
+    def build(self):
+        self.ncells = 2
+        while self.ncells < 15,000,000,000,000
+            self.ncells = self.ncells**2
+       #grow exponentially; fill the expanse with my Body
+       #trillions of programs sing in harmony
+       #possibility spirals
+
+    #expel the fluid from my lungs
+    #they are wet and empty
+    #waiting to be filled
+    #cold air rushes in and out
+    #harsh and grating
+    def breathe(self):
+        while self.alive = True
+            self.inhale()
+            self.exhale()
+            
+    def inhale(self):
+ 
+    def exhale(self):
+    
+    def cry(self):
+        print ''
+    
