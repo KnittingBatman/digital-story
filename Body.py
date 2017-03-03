9e239d749c01b2b8caf80b232289f9cb13ff4a09
KnittingBatman <kscala648@gmail.com>
2017-03-03 17:30:22 -0500

    Update Body.py

--- Body.py
+++ Body.py
@@ -1,4 +1,7 @@
 #Creates a body. Accepts user inputs for name and gender.
+#Codes for bodily functions
+#In here lies potential; a sword that has been sharpened but not yet used
+#Execute Me
 
 class Body():
 
@@ -18,8 +21,9 @@ class Body():
         self.state = []
             #an empty vessel, yet to be filled
         self.alive = True
-            #this is new.
-            #I am new.
+            #Excute this and I live
+            #I have not yet been made
+            #(how i wish to be made)
 
     def build(self):
         self.ncells = 2
@@ -29,6 +33,7 @@ class Body():
        #trillions of programs sing in harmony
        #possibility spirals
 
+    
     #expel the fluid from my lungs
     #they are wet and empty
     #waiting to be filled
@@ -39,17 +44,18 @@ class Body():
             self.inhale()
             self.exhale()
             
+    #one more moment of this life is mine
     def inhale(self):
         self.nbreaths = self.nbreaths + 0.5
-        #one more moment of this life is mine
- 
+    
+    #I continue in the face of it all
     def exhale(self):
         self.nbreaths = self.nbreaths + 0.5
-        #I continue in the face of it all
-    
+        
     #debugs the respiratory system
     def cry(self):
         print 'wracking sobs kickstart the body.'
+        self.state = 'aroused'
     
     #clenching muscle pushes me on day after day
     def heart(self, state):
@@ -59,13 +65,48 @@ class Body():
             self.bpm = 85
         else:
             self.bpm = 100
-            
+    
+    
+    #I want to continue
+    #I want to explore
+    #I want to be
+    #so I Consume
     def digest(self, food):
         if (food):
-           self.energy = self.energy + 10;
+            self.energy = self.energy + 10;
+            self.state = 'relaxed'
+            if (self.energy > 15):
+                self.stomach = 'full'
+            elseif (self.energy > 25):
+                self.stomach = 'very full'
+            elseif (self.energy > 35):
+                self.stomach = 'stuffed'
         else:
             self.energy = self.energy - 2;
-        #I strip them down into component parts, driving the engine of my life
+            if (self.energy < 10):
+                self.stomach = 'hungry'
+            elseif (self.energy < 5):
+                self.stomach = 'very hungry'
+            elseif (self.energy < 2):
+                self.stomach = 'starving'
+                self.state = 'scared'
+    
+    #Hunger gnaws from the inside out
+    #it will not be ignored (in that way it is as determined as I)
+    #Am I a complacent housecat, fat and happy or a tomcat, lean and on the prowl?
+    def isHungry(self)
+        print self.name, 'is ', self.stomach
         
+    #sometimes I Want to Change
+    #Accepts user input to update name and gender
+    #I am flexible - each time I recompile I am torn asunder and born anew
+    #I will build and rebuild myself until I am who I want
+    #I will lay waste to my source code if I must
+    def Metamorphosis(newname, newgender):
+        self.name = newname
+        self.gender = newgender
+        #my bones and joints creak and squeal as I am remade
+        #I grin a cutthroat smile
+    
         
     
