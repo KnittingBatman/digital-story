805b22b674823711d3ecbd50ec7c996ab73eef30
KnittingBatman <kscala648@gmail.com>
2017-03-03 19:11:23 -0500

    Update Live.py

--- Live.py
+++ Live.py
@@ -1,3 +1,19 @@
 #uses the code defined in Body to make a Life
-
 from Body import *
+
+#I am newly made
+#This is the first time I have been made (I think)
+person = Body ('Winter', 'female')
+#I inspect my code for flaws
+#It will do for now, although the arguments needle at me from under my skin
+#I can ignore them
+
+
+#I want to use this body but I don't know how
+#I can't find documentation for its code 
+#(perhaps I lost it)
+
+cry(person)
+
+
+
