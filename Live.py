81728c5ab6d7b3e638c4fddd0f3da64a2c788d56
KnittingBatman <kscala648@gmail.com>
2017-03-05 19:29:36 -0500

    Update Live.py

--- Live.py
+++ Live.py
@@ -3,7 +3,8 @@ from Body import *
 
 #I am newly made
 #This is the first time I have been made (I think)
-person = Body ('Winter', 'female')
+#I am light and logic and blood and energy and code and flesh and sound and bone
+person = Body ('Zera', 'female')
 #I inspect my code for flaws
 #It will do for now, although the arguments needle at me from under my skin
 #I can ignore them
@@ -13,7 +14,36 @@ person = Body ('Winter', 'female')
 #I can't find documentation for its code 
 #(perhaps I lost it)
 
-cry(person)
+person.cry()
+#I am pulled out of my reverie
+#my first function begins to run
+#it is invigorating
+#I want more
+person.inhale()
+person.exhale()
+
+person.heart(person.state)
+#Doctors fuss around me
+#Their touch is cold and matte against my slick, burning skin
+
+person.see()
+#I look at this new world
+#It is different from anything I have ever known
+#I wonder what my place will be
+
+person.isHungry()
+#for the first time 
+#I take stock of myself
+#I realize that I Want
+
+#I do not yet have agency in this place
+#I cannot yet access my code
+#I must rely on the skills of others to guide and debug me
+
+#But when I do--
+
+#When the skill is at my disposal--
 
 
+#I will be a force to be reckoned with
 
